12036c19922926d70a8348417de2639e54c63294
ARP <andrewarp06@gmail.com>
2026-04-13 13:20:47 +0200

    ejercicio2
    ¿Lo subo en .txt? La próxima vez lo meto directamente en un captura de Minecraft grabado en piedra con un libro y pluma

--- ejercicio2/Main.java
+++ ejercicio2/Main.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package actividades.ud9_excepciones;
+package ejecicio2;
 import java.io.* ;
 /**
  *
