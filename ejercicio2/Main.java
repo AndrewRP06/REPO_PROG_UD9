27b3f15dec281de9ecac3fcb7b8536d10a30167b
ARP <andrewarp06@gmail.com>
2026-04-13 13:17:48 +0200

    EJERCICIO 2
    lo subo en txt? A la proximo lo subo en un libro del minecraft

---
+++ ejercicio2/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package actividades.ud9_excepciones;
+import java.io.* ;
+/**
+ *
+ * @author ALUMNO
+ */
+public class Main {
+    public static void main(String[] args) {
+        try (FileReader frbro = new FileReader("C:\\Users\\ALUMNO\\Documents\\NetBeansProjects\\UD4_Tablas\\src\\main\\java\\a01\\A01.java")) {
+                int carac;
+             while ((carac = frbro.read()) != -1) {
+                System.out.print((char) carac);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
